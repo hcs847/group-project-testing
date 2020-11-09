799b6dd58b9622056d9f37932aa06efe32bf3d7a
Hila Caspi <caspi.home@gmail.com>
2020-11-09 15:21:37 -0800

    removing debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -243,7 +243,7 @@ displayPreviousStockTickers(stockTickers);
 renderStoredStockTickers();
 
 // calling the news api to render market news in the footer
-// temp commenting out =======>  getRapidApiNews();
+getRapidApiNews();
 
 // Event Listener for the search icon, when clicked will run the getCompanyData function to display stock information.
 $(document).on("click", ".search-icon", function () {
